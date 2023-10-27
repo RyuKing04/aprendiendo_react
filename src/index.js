5298cc8693677070a80a630c43540797f9504273
RyuKing04 <tonyramirezs135@gmail.com>
2023-10-27 01:27:15 -0600

    Primer proyecto React puro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const subameElSalario="Subame el salario";
+const color= 'red';  
+const sesion=true;
+const pais='Costa Rica';
+const amigos= ['Juan', 'Pedro', 'Pablo'];
+
+const Usuario=() => {
+
+  return(
+    <div>
+    <h1>Inicio de sesion exitoso</h1>
+    <h1 className='titulo' style={{color: color}}>Hello World</h1>
+  <p>Lets render this to the DOM</p>
+  <h2>{subameElSalario} Por favor </h2>
+  {pais && <p>El pais es:  {pais}</p>}
+  <ul>
+{amigos.map((amigos, index)=><li key={index}>{amigos}  
+</li>
+)}
+    </ul>
+    <p>tkm</p>
+    </div>
+
+  );
+};
+
+const App= ()=>{
+return(
+
+ <>
+    {sesion===true ? <Usuario/> : <h1>Por favor inicie sesion</h1>}
+    </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+  
+// const verificarSesion=(sesion)=>{
+//   if(sesion===true){
+//     return JSX;
+//   } else{
+//     return <h1>Por favor inicie sesion</h1>
+//   }
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {verificarSesion(sesion)}
+//   </React.StrictMode>
+// );
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App/>
+   </React.StrictMode>
+ );
+
